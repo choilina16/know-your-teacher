ab7eb925add84f4fb58790c63ec4660a524aa194
S.Snavely <info@stevesnavely.com>
2022-05-03 11:49:47 -0400

    Updated student/ user seed files

---
+++ seeds/studentData.js
@@ -0,0 +1,28 @@
+const { Student } = require("../models");
+
+const studentData = [
+  {
+    first_name: "Colette",
+    last_name: "Koala",
+  },
+  {
+    first_name: "Cricket",
+    last_name: "Pearl",
+  },
+  {
+    first_name: "Elsie",
+    last_name: "Otter",
+  },
+  {
+    first_name: "Halcyon",
+    last_name: "Juna",
+  },
+  {
+    first_name: "Mirabella",
+    last_name: "Bunny",
+  },
+];
+
+const seedStudent = () => Student.bulkCreate(studentdata);
+
+module.exports = seedStudent;
