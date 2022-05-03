608e7d8f0cfd389dabae79d9a5b250628a9e86c6
sebastian Chunley <sebastian.chunley@gmail.com>
2022-05-03 11:22:52 -0400

    modelUpdate

--- models/teacher.js
+++ models/teacher.js
@@ -1,65 +1,55 @@
-const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
 
-class Teacher extends Model {
+class User extends Model {
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
   }
 }
 
-Teacher.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    first_name: {
-      type: DataTypes.STRING.VARCHAR(30),
-      allowNull: false,
-    },
-    last_name: {
-      type: DataTypes.STRING.VARCHAR(30),
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [8],
-      },
-    },
-    teacher_confirm: {
-      type: DataTypes.BOOLEAN,
-    },
+Teacher.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
   },
-  {
-    hooks: {
-      beforeCreate: async (newTeacherData) => {
-        newTeacherData.password = await bcrypt.hash(newTeacherData.password, 10);
-        return newTeacherData;
-      },
-      beforeUpdate: async (updatedTeacherData) => {
-        updatedTeacherData.password = await bcrypt.hash(updatedTeacherData.password, 10);
-        return updatedTeacherData;
-      },
-    },
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'teacher',
-  }
-);
-
-module.exports = Teacher;
+  first_name: {
+    type: DataTypes.STRING.VARCHAR(30),
+    allowNull: false,
+  },
+  last_name: {
+    type: DataTypes.STRING.VARCHAR(30),
+    allowNull: false,
+  },
+  color: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  flower: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  candy: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  places_to_eat: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  hobbies: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  other: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: "teacher",
+});
