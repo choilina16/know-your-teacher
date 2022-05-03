b574e1e02f11c97df1a8a746ad70a20d637c0726
sebastian Chunley <sebastian.chunley@gmail.com>
2022-05-03 14:04:46 -0400

    addingColorVar

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,24 +1,24 @@
-const sequelize = require('../config/connection');
-const { Student, Teacher, User } = require('../models');
+const sequelize = require("../config/connection");
+const { Student, Teacher, User } = require("../models");
 
-// connecting to the .json files 
-const seedTeacher = require('./teacherData.json');
-const seedStudent = require('./studentData.json');
-const seedUser = require('./userData.json');
+// connecting to the .json files
+const seedTeacher = require("./teacherData.json");
+const seedStudent = require("./studentData.json");
+const seedUser = require("./userData.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
-  console.log('\n----- DATABASE SYNCED -----\n');
+  console.log("\n----- DATABASE SYNCED -----\n");
 
   await seedTeacher();
-  console.log('\n----- TEACHERS SEEDED -----\n');
+  console.log("\n----- TEACHERS SEEDED -----\n");
 
   await seedStudent();
-  console.log('\n----- STUDENTS SEEDED -----\n');
+  console.log("\n----- STUDENTS SEEDED -----\n");
 
   await seedUser();
-  console.log('\n----- USERS SEEDED -----\n');
-  
+  console.log("\n----- USERS SEEDED -----\n");
+
   process.exit(0);
 };
 
