df2c09f8cbed0bc96b609c6825e910095eee21cd
S.Snavely <info@stevesnavely.com>
2022-05-05 15:06:05 -0400

    updated login code for bcrypt edit

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,9 +1,10 @@
-const router = require('express').Router();
-const { User } = require('../../models');
+const router = require("express").Router();
+const { User } = require("../../models");
+const bcrypt = require("bcrypt");
 // const withAuth = require('../../utils/auth');
 
 // Create a user -> /api/users/
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const newUser = await User.create({
       email: req.body.email,
@@ -23,38 +24,47 @@ router.post('/', async (req, res) => {
 });
 
 //login to an account -> /api/users/login
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   // checking to see the email matches the db
   try {
     const user = await User.findOne({ where: { email: req.body.email } });
 
     if (!user) {
-      res.status(400).json({ message: 'Incorrect email please try again' });
+      res.status(400).json({ message: "Incorrect email please try again" });
       return;
     }
     // checking to see if the ps matches
-    const validPassword = await user.checkPassword(req.body.password);
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
 
     if (!validPassword) {
-      res.status(400).json({ message: 'Incorrect password, please try again' });
+      res.status(400).json({ message: "Incorrect password, please try again" });
       return;
     }
-    // creates a sesson with the express session library
-    req.session.save(() => {
-      // req.session.email = user.email;
-      // req.session.password = user.password;
-      req.session.user_id = user.id;
-      req.session.logged_in = true;
-
-      res.json({ user: user, message: 'You are now logged in!' });
-    });
+    // if they do match, return success message
+    res.status(200).json({ message: "You are now logged in!" });
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
+  // // creates a sesson with the express session library
+  // req.session.save(() => {
+  //   // req.session.email = user.email;
+  //   // req.session.password = user.password;
+  //   req.session.user_id = user.id;
+  //   req.session.logged_in = true;
+
+  //   res.json({ user: user, message: 'You are now logged in!' });
+  // });
+
+  //  catch (err) {
+  //   res.status(400).json(err);
+  // };
 });
 
 // ends session
-router.post('/logout', (req, res) => {
+router.post("/logout", (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
       res.status(204).end();
--- public/js/login.js
+++ public/js/login.js
@@ -2,29 +2,29 @@ const loginFormHandler = async (event) => {
   event.preventDefault();
 
   // targeting the label for= for pw & username
-  const emailEl = document.querySelector('#email-login');
-  const passwordEl = document.querySelector('#password-login');
+  const emailEl = document.querySelector("#email-login");
+  const passwordEl = document.querySelector("#password-login");
 
   if (emailEl && passwordEl) {
-    const response = await fetch('/api/users/login', {
-      method: 'POST',
+    const response = await fetch("/api/users/login", {
+      method: "POST",
       body: JSON.stringify({
         email: emailEl.value,
         password: passwordEl.value,
       }),
 
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
     });
 
     // If successful, redirect the browser to the create page
     if (response.ok) {
-      document.location.replace('/create');
+      document.location.replace("/teacher");
     } else {
-      alert('Failed to login!');
+      alert("Failed to login!");
     }
   }
 };
 
 document
-  .querySelector('#login-form')
-  .addEventListener('submit', loginFormHandler);
+  .querySelector("#login-form")
+  .addEventListener("submit", loginFormHandler);
