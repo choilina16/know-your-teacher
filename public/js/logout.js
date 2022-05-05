f7065e2bc7f6f21bd78d9e9b87155f9e28e62010
S.Snavely <info@stevesnavely.com>
2022-05-04 20:25:09 -0400

    Updated login/logout feature

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,15 @@
 const logout = async () => {
-  const response = await fetch('/api/users/logout', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+  const response = await fetch("/api/users/logout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
   });
 
   if (response.ok) {
-    document.location.replace('/');
+    document.location.replace("/login");
   } else {
-    alert(response.statusText);
+    // alert(response.statusText); **SS 5/4 replaced with other alert to test
+    alert("Failed to log in");
   }
 };
 
-document.querySelector('#logout').addEventListener('click', logout);
+document.querySelector("#logout").addEventListener("click", logout);
